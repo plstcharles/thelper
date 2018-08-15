8ba1d8a6a3a27169f4d6bae35a23fa1bef238011
Pierre-Luc St-Charles <stcharpl@crim.ca>
2018-08-15 13:05:29 -0400

    Add session name prefix to tbx log output

--- src/thelper/train.py
+++ src/thelper/train.py
@@ -70,9 +70,9 @@ class Trainer:
             self.use_tbx = thelper.utils.str2bool(config["use_tbx"])
         writer_paths = [None, None, None]
         if self.use_tbx:
-            self.tbx_root_dir = os.path.join(self.save_dir, "tbx_logs")
+            self.tbx_root_dir = os.path.join(self.save_dir, "tbx_logs", self.name)
             if not os.path.exists(self.tbx_root_dir):
-                os.mkdir(self.tbx_root_dir)
+                os.makedirs(self.tbx_root_dir)
             timestr = time.strftime("%Y%m%d-%H%M%S")
             train_foldername = "train-%s-%s" % (platform.node(), timestr)
             valid_foldername = "valid-%s-%s" % (platform.node(), timestr)
