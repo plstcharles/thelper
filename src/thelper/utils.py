6673bb2c552d55d27023105b6c1b663779563e44
Pierre-Luc St-Charles <stcharpl@crim.ca>
2018-11-07 11:57:32 -0500

    Cleanup old todos from ext sources

--- src/thelper/utils.py
+++ src/thelper/utils.py
@@ -121,7 +121,6 @@ def get_caller_name(skip=2):
     name = []
     module = inspect.getmodule(parentframe)
     # `modname` can be None when frame is executed directly in console
-    # TODO(techtonik): consider using __main__
     if module:
         name.append(module.__name__)
     # detect classname
