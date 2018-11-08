a41e6269a7eddf9dccff16575260a17a481e0166
Pierre-Luc St-Charles <stcharpl@crim.ca>
2018-11-08 18:06:05 -0500

    Cleanup modules subpkg imports

--- thelper/modules/__init__.py
+++ thelper/modules/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-import thelper.modules.utils
-from thelper.modules.utils import Module
-from thelper.modules.utils import load_model
+import thelper.modules.utils  # noqa: F401
+from thelper.modules.utils import Module  # noqa: F401
+from thelper.modules.utils import load_model  # noqa: F401
 
 logger = logging.getLogger("thelper.modules")
