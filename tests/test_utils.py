f899071a47e8553c2ee7807995be1fc919557486
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-12-11 16:26:21 -0500

    auto apply 'cpu' on none gpu available

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+import thelper
+import thelper.utils as tu
+# noinspection PyPackageRequirements
+import mock
+
+
+# noinspection PyUnusedLocal
+@mock.patch('thelper.utils.get_available_cuda_devices', return_value=[])    # no device available
+@mock.patch('torch.load', return_value={'version': thelper.__version__})
+def test_load_checkpoint_default_cpu_no_devices(mock_torch_load, mock_thelper_devices):
+    checkpoint_file = 'dummy-checkpoint.pth'
+    tu.load_checkpoint(checkpoint_file)
+    mock_torch_load.assert_called_once_with(checkpoint_file, map_location='cpu')
+
+
+# noinspection PyUnusedLocal
+@mock.patch('thelper.utils.get_available_cuda_devices', return_value=[0])   # one device available
+@mock.patch('torch.load', return_value={'version': thelper.__version__})
+def test_load_checkpoint_not_default_cpu_with_devices(mock_torch_load, mock_thelper_devices):
+    checkpoint_file = 'dummy-checkpoint.pth'
+    tu.load_checkpoint(checkpoint_file)
+    mock_torch_load.assert_called_once_with(checkpoint_file, map_location=None)
