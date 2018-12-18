96971afa9459661622315c642c50adb6f5f2a784
Pierre-Luc St-Charles <stcharpl@crim.ca>
2018-12-17 19:17:41 -0500

    Add missing classes to optim pkg init

--- thelper/optim/__init__.py
+++ thelper/optim/__init__.py
@@ -15,6 +15,8 @@ from thelper.optim.metrics import ClassifLogger  # noqa: F401
 from thelper.optim.metrics import ClassifReport  # noqa: F401
 from thelper.optim.metrics import ConfusionMatrix  # noqa: F401
 from thelper.optim.metrics import ExternalMetric  # noqa: F401
+from thelper.optim.metrics import MeanAbsoluteError  # noqa: F401
+from thelper.optim.metrics import MeanSquaredError  # noqa: F401
 from thelper.optim.metrics import Metric  # noqa: F401
 from thelper.optim.metrics import RawPredictions  # noqa: F401
 from thelper.optim.metrics import ROCCurve  # noqa: F401
