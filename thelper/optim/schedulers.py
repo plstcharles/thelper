2077dad262e2106be47b8534e5237bc614558701
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-01-22 18:59:51 -0500

    Fixup custom scheduler step indexing

--- thelper/optim/schedulers.py
+++ thelper/optim/schedulers.py
@@ -18,7 +18,8 @@ class CustomStepLR(torch.optim.lr_scheduler._LRScheduler):
     for epoch-based milestones.
 
     This class can be useful for tuning the learning rate scheduling behavior of a training session
-    beyond what is already possible using PyTorch's existing LR scheduler classes.
+    beyond what is already possible using PyTorch's existing LR scheduler classes.  Note that all
+    epoch indices are assumed to be 0-based.
 
     Usage example in Python::
 
@@ -29,8 +30,8 @@ class CustomStepLR(torch.optim.lr_scheduler._LRScheduler):
         # lr = 0.05      if 4 <= epoch < 30  (default scale between epoch 4 and 30)
         # lr = 0.005     if 30 <= epoch < 80 (1/10 scale past epoch 30)
         # lr = 0.0005    if epoch >= 80      (1/100 scale past epoch 80)
-        scheduler = CustomStepLR(optimizer, milestone_map={
-            1: 1/8,
+        scheduler = CustomStepLR(optimizer, milestones={
+            0: 1/8,
             2: 1/4,
             3: 1/2,
             4: 1,
@@ -83,8 +84,8 @@ class CustomStepLR(torch.optim.lr_scheduler._LRScheduler):
     def __init__(self, optimizer, milestones, last_epoch=-1):
         """Receives the optimizer, milestone scaling factor, and initialization state.
 
-        If the milestones do not include the first epoch, then its scaling factor is set to 1. When
-        last_epoch is -1, the training is assumed to start from scratch.
+        If the milestones do not include the first epoch (idx = 0), then its scaling factor is set
+        to 1. When last_epoch is -1, the training is assumed to start from scratch.
 
         Args:
             optimizer: Wrapped optimizer (PyTorch-compatible object).
@@ -106,13 +107,19 @@ class CustomStepLR(torch.optim.lr_scheduler._LRScheduler):
             if not isinstance(list(milestones.values())[0], (float, int)):
                 raise AssertionError("milestone scaling factors should be int/float")
         self.scales = [float(scale) for scale in milestones.values()]
-        if 1 not in self.stages:
+        if 0 not in self.stages:
             self.stages.insert(0, int(1))
             self.scales.insert(0, float(1))
         self.milestones = milestones
         super().__init__(optimizer, last_epoch)
 
+    def _get_stage_idx(self, epoch):
+        if epoch in self.stages:
+            return self.stages.index(epoch)
+        return max(bisect.bisect_right(self.stages, epoch) - 1, 0)
+
     def get_lr(self):
         """Returns the learning rate to use given the current epoch and scaling factors."""
-        stage = min(bisect.bisect_right(self.stages, self.last_epoch), len(self.stages) - 1)
-        return [base_lr * self.scales[stage] for base_lr in self.base_lrs]
+        scale = self.scales[self._get_stage_idx(self.last_epoch)]
+        print("found scale = %s closest at epoch = %s" % (str(scale), str(self.last_epoch)))
+        return [base_lr * scale for base_lr in self.base_lrs]
