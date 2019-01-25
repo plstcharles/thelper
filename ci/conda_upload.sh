41cefff086cd68f5e2c0581119c3f6e84b73e22c
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-01-25 00:57:47 -0500

    Add travis conda deployment stage & upload

---
+++ ci/conda_upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CONDA_BUILD_PATH=~/conda-build
+
+mkdir $CONDA_BUILD_PATH
+conda config --set anaconda_upload no
+conda build .
+anaconda -t $CONDA_TOKEN upload -u $CONDA_USERNAME \
+    $CONDA_BUILD_PATH/$TRAVIS_OS_NAME-64/thelper-0.2.1.tar.bz2 --force
