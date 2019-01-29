11ba0e94ec0e82dab2fbe6f14c2d657639fcbb3d
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-01-29 17:12:49 -0500

    Cleanup travis/conda build upload env variables

--- ci/conda_upload.sh
+++ ci/conda_upload.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-export CONDA_BUILD_PATH=~/conda-build
-
-mkdir $CONDA_BUILD_PATH
 conda config --set anaconda_upload no
 conda build .
 anaconda -t $CONDA_TOKEN upload -u $CONDA_USERNAME \
-    $CONDA_BUILD_PATH/$TRAVIS_OS_NAME-64/thelper-0.2.4.tar.bz2 --force
+    $CONDA_BUILD_PATH/thelper-0.2.4$CONDA_BUILD_SUFFIX.tar.bz2 --force
