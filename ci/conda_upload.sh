a45b9361ed7ee9fd4fc2d7b2fe0380a3357b5b6a
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-02-04 16:38:59 -0500

    Update conda build variants w/ auto upload

--- ci/conda_upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-conda config --set anaconda_upload no
-conda build .
-anaconda -t $CONDA_TOKEN upload -u $CONDA_USERNAME \
-    $CONDA_BUILD_PATH/thelper-0.2.6$CONDA_BUILD_SUFFIX.tar.bz2 --force
