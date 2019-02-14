8bbcddf6b06ef9092e501aabc25599f3be566c65
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-02-13 23:35:07 -0500

    Fixup dataset string repr arg ordering

--- thelper/data/parsers.py
+++ thelper/data/parsers.py
@@ -110,7 +110,7 @@ class Dataset(torch.utils.data.Dataset):
     def __repr__(self):
         """Returns a print-friendly representation of this dataset."""
         return self._get_derived_name() + ": {{\n\tsize: {},\n\tdeepcopy: {},\n\ttransforms: {}\n}}".format(
-            str(len(self)), str(self.transforms), str(self.deepcopy)
+            str(len(self)), str(self.deepcopy), str(self.transforms)
         )
 
 
