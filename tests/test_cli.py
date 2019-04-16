813100973a2489e01449ded37a0d266e09f3baa5
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-04-16 13:47:05 -0400

    Add viz/split/annot cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@ import copy
 import os
 import shutil
 
+import numpy as np
 import pytest
 
 import thelper
@@ -21,7 +22,7 @@ def simple_config(request):
     request.addfinalizer(fin)
     os.makedirs(test_create_simple_images_path, exist_ok=True)
     for cls in range(10):
-        os.makedirs(os.path.join(test_create_simple_images_path, str(cls)))
+        os.makedirs(os.path.join(test_create_simple_images_path, str(cls)), exist_ok=True)
         for idx in range(10):
             open(os.path.join(test_create_simple_images_path, str(cls), str(idx) + ".jpg"), "a").close()
     return {
@@ -116,6 +117,10 @@ def test_resume_session(simple_config, mocker):
     ckptdata_fake = {key: val for key, val in ckptdata.items() if key != "task"}
     with pytest.raises(AssertionError):
         thelper.cli.resume_session(ckptdata_fake, test_save_path, eval_only=True)
+    nameless_ckptdata = copy.deepcopy(ckptdata)
+    del nameless_ckptdata["config"]["name"]
+    with pytest.raises(AssertionError):
+        thelper.cli.resume_session(nameless_ckptdata, test_save_path, eval_only=True)
     thelper.cli.resume_session(ckptdata, test_save_path, eval_only=True)
     assert fake_train.call_count == 2
     assert fake_eval.call_count == 3
@@ -189,3 +194,116 @@ def test_visualize_data(simple_config, mocker):
     thelper.cli.visualize_data(simple_config)
     assert fake_draw.call_count == 104
     assert fake_imread.call_count == 200
+
+
+@pytest.fixture
+def annot_config(request):
+    def fin():
+        shutil.rmtree(test_create_simple_path, ignore_errors=True)
+        shutil.rmtree(test_create_simple_images_path, ignore_errors=True)
+    fin()
+    request.addfinalizer(fin)
+    os.makedirs(test_create_simple_images_path, exist_ok=True)
+    for cls in range(10):
+        os.makedirs(os.path.join(test_create_simple_images_path, str(cls)), exist_ok=True)
+        for idx in range(10):
+            open(os.path.join(test_create_simple_images_path, str(cls), str(idx) + ".jpg"), "a").close()
+    return {
+        "name": "simple",
+        "bypass_queries": True,
+        "datasets": {
+            "output": {
+                "type": "thelper.data.ImageFolderDataset",
+                "params": {
+                    "root": test_create_simple_images_path
+                }
+            }
+        },
+        "annotator": {
+            "type": "thelper.gui.ImageSegmentAnnotator",
+            "params": {
+                "sample_input_key": "image",
+                "labels": [
+                    {"id": 255, "name": "foreground", "color": [0, 0, 255]}
+                ]
+            }
+        }
+    }
+
+
+def test_annotate_data(annot_config, mocker):
+    import thelper.gui
+    fake_run = mocker.patch.object(thelper.gui.ImageSegmentAnnotator, "run")
+    fake_imread = mocker.patch("cv2.imread", return_value=np.random.randint(0, 255, (256, 256, 3), dtype=np.uint8))
+    _ = mocker.patch("cv2.setMouseCallback")
+    _ = mocker.patch("cv2.namedWindow")
+    _ = mocker.patch("cv2.waitKey")
+    _ = mocker.patch("cv2.imshow")
+    _ = mocker.patch("pynput.keyboard.Listener")
+    fake_config = copy.deepcopy(annot_config)
+    del fake_config["name"]
+    with pytest.raises(AssertionError):
+        thelper.cli.annotate_data(fake_config, test_save_path)
+    assert fake_run.call_count == 0
+    thelper.cli.annotate_data(annot_config, test_save_path)
+    assert fake_run.call_count == 1
+    assert fake_imread.call_count == 1
+
+
+@pytest.fixture
+def split_config(request):
+    def fin():
+        shutil.rmtree(test_create_simple_path, ignore_errors=True)
+    fin()
+    request.addfinalizer(fin)
+    os.makedirs(test_create_simple_images_path, exist_ok=True)
+    for cls in range(10):
+        os.makedirs(os.path.join(test_create_simple_images_path, str(cls)), exist_ok=True)
+        for idx in range(10):
+            open(os.path.join(test_create_simple_images_path, str(cls), str(idx) + ".jpg"), "a").close()
+    return {
+        "name": "simple",
+        "bypass_queries": True,
+        "datasets": {
+            "dset": {
+                "type": "thelper.data.ImageFolderDataset",
+                "params": {
+                    "root": test_create_simple_images_path
+                }
+            }
+        },
+        "loaders": {
+            "shuffle": True,
+            "workers": 0,
+            "batch_size": 1,
+            "skip_class_balancing": True,
+            "train_split": {
+                "dset": 0.9
+            },
+            "valid_split": {
+                "dset": 0.1
+            }
+        }
+    }
+
+
+def test_split_data(split_config, mocker):
+    fake_config = copy.deepcopy(split_config)
+    fake_create = mocker.patch("thelper.data.create_hdf5")
+    del fake_config["name"]
+    with pytest.raises(AssertionError):
+        thelper.cli.split_data(fake_config, test_save_path)
+    fake_config = copy.deepcopy(split_config)
+    del fake_config["loaders"]
+    with pytest.raises(AssertionError):
+        thelper.cli.split_data(fake_config, test_save_path)
+    fake_config = copy.deepcopy(split_config)
+    fake_config["split"] = "dummy"
+    with pytest.raises(AssertionError):
+        thelper.cli.split_data(fake_config, test_save_path)
+    fake_config = copy.deepcopy(split_config)
+    fake_config["split"] = {"compression": "dummy"}
+    with pytest.raises(AssertionError):
+        thelper.cli.split_data(fake_config, test_save_path)
+    thelper.cli.split_data(split_config, test_save_path)
+    assert fake_create.call_count == 1
