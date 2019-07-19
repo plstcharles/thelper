153d9b7ec7b1a0b81b5f9bd8078a6dc5030e443a
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-07-19 18:32:24 +0000

    Add train tests for session outputs and callbacks

--- tests/train/test_classif_mnist.py
+++ tests/train/test_classif_mnist.py
@@ -1,8 +1,10 @@
 import copy
+import json
 import os
 import shutil
 
 import pytest
+import torch
 
 import thelper
 
@@ -81,7 +83,6 @@ def config(request):
             "type": "thelper.nn.lenet.LeNet"
         },
         "trainer": {
-            "type": "thelper.train.ImageClassifTrainer",
             "epochs": 2,
             "monitor": "accuracy",
             "optimization": {
@@ -107,7 +108,7 @@ def config(request):
     }
 
 
-def test_classif_mnist(config):
+def test_reload(config):
     train_outputs = thelper.cli.create_session(config, test_save_path)
     assert len(train_outputs) == 2
     assert train_outputs[0]["train/metrics"]["accuracy"] < train_outputs[1]["train/metrics"]["accuracy"]
@@ -126,3 +127,91 @@ def test_classif_mnist(config):
     finetune_outputs = thelper.cli.create_session(override_config, test_save_path)
     assert len(finetune_outputs) == 1
     assert finetune_outputs[0]["train/metrics"]["accuracy"] > train_outputs[1]["train/metrics"]["accuracy"]
+
+
+def compare_dictionaries(dictA, dictB, dictA_name="A", dictB_name="B", path=""):
+    err, key_err, value_err = "", "", ""
+    old_path = path
+    for k in dictA.keys():
+        path = old_path + "[%s]" % k
+        if k not in dictB:
+            key_err += "key %s%s not in %s\n" % (dictB_name, path, dictB_name)
+        else:
+            if isinstance(dictA[k], dict) and isinstance(dictB[k], dict):
+                err += compare_dictionaries(dictA[k], dictB[k], dictA_name, dictB_name, path)
+            else:
+                if dictA[k] != dictB[k]:
+                    value_err += "value of %s%s (%s) not same as %s%s (%s)\n"\
+                        % (dictA_name, path, dictA[k], dictB_name, path, dictB[k])
+    for k in dictB.keys():
+        path = old_path + "[%s]" % k
+        if k not in dictA:
+            key_err += "key %s%s not in %s\n" % (dictB_name, path, dictA_name)
+    return key_err + value_err + err
+
+
+def test_outputs(config):
+    override_config = copy.deepcopy(config)
+    override_config["trainer"]["use_tbx"] = True
+    train_outputs = thelper.cli.create_session(override_config, test_save_path)
+    assert len(train_outputs) == 2
+    assert train_outputs[0]["train/metrics"]["accuracy"] < train_outputs[1]["train/metrics"]["accuracy"]
+    output_path = os.path.join(test_classif_mnist_path, "output", test_classif_mnist_name)
+    assert os.path.isdir(output_path)
+    out_dirs = next(os.walk(output_path))[1]
+    assert len(out_dirs) == 2
+    assert any([out_dir.startswith("train-") for out_dir in out_dirs])
+    assert any([out_dir.startswith("valid-") for out_dir in out_dirs])
+    for out_dir in out_dirs:
+        ltype = "train" if out_dir.startswith("train-") else "valid"
+        epoch_out_path = os.path.join(output_path, out_dir)
+        assert os.path.isdir(epoch_out_path)
+        epoch_out_files = next(os.walk(epoch_out_path))[2]
+        assert len(epoch_out_files) == 4
+        assert "accuracy-0000.txt" in epoch_out_files
+        assert "accuracy-0001.txt" in epoch_out_files
+        assert "config.json" in epoch_out_files
+        assert any([p.startswith("events.out.tfevents.") for p in epoch_out_files])
+        for filename in epoch_out_files:
+            if filename.startswith("accuracy-"):
+                epoch = int(filename.split("-")[1].split(".")[0])
+                with open(os.path.join(epoch_out_path, filename), "r") as fd:
+                    assert float(fd.readline()) == train_outputs[epoch][ltype + "/metrics"]["accuracy"]
+            elif filename == "config.json":
+                with open(os.path.join(epoch_out_path, filename), "r") as fd:
+                    backup_config = json.load(fd)
+                    assert compare_dictionaries(backup_config, override_config) == ""
+
+
+def callback(task,  # type: thelper.tasks.utils.Task
+             input,  # type: thelper.typedefs.InputType
+             pred,  # type: thelper.typedefs.PredictionType
+             target,  # type: thelper.typedefs.TargetType
+             sample,  # type: thelper.typedefs.SampleType
+             iter_idx,  # type: int
+             max_iters,  # type: int
+             epoch_idx,  # type: int
+             max_epochs,  # type: int
+             **kwargs):
+    assert isinstance(task, thelper.tasks.Classification)
+    assert isinstance(input, torch.Tensor)
+    assert isinstance(pred, torch.Tensor)
+    assert isinstance(target, torch.Tensor)
+    assert isinstance(sample, dict)
+    assert isinstance(iter_idx, int)
+    assert isinstance(max_iters, int) and iter_idx < max_iters
+    assert isinstance(epoch_idx, int)
+    assert isinstance(max_epochs, int) and epoch_idx < max_epochs
+    assert "hello" in kwargs
+    kwargs["hello"][0] = "bye"
+
+
+def test_callbacks(config, mocker):
+    override_config = copy.deepcopy(config)
+    override_config["trainer"]["callback"] = callback
+    override_config["trainer"]["callback_kwargs"] = {"hello": ["hi"]}
+    override_config["trainer"]["display"] = True
+    fake_draw = mocker.patch("thelper.utils.draw")
+    assert thelper.cli.create_session(override_config, test_save_path)
+    assert fake_draw.call_count > 0
+    assert override_config["trainer"]["callback_kwargs"]["hello"][0] == "bye"
