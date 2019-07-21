4d60b1d15758013e8b398dbd54e97c1a3742ec1a
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-07-21 20:57:00 +0000

    Add new properties to bbox impl, cleanup existing

--- thelper/tasks/detect.py
+++ thelper/tasks/detect.py
@@ -28,11 +28,11 @@ class BoundingBox:
     Attributes:
         class_id: type identifier for the underlying object instance.
         bbox: four-element tuple holding the (xmin,ymin,xmax,ymax) bounding box parameters.
+        include_margin: defines whether xmax/ymax is included in the bounding box area or not.
         difficult: defines whether this instance is considered "difficult" (false by default).
         occluded: defines whether this instance is considered "occluded" (false by default).
         truncated: defines whether this instance is considered "truncated" (false by default).
         iscrowd: defines whether this instance covers a "crowd" of objects or not (false by default).
-        area: scalar indicating the total surface of the annotation (will be computed automatically if None).
         confidence: scalar or array of prediction confidence values tied to class types (empty by default).
         image_id: string used to identify the image containing this bounding box (i.e. file path or uuid).
         task: reference to the task object that holds extra metadata regarding the content of the bbox (None by default).
@@ -42,17 +42,17 @@ class BoundingBox:
         | :class:`thelper.tasks.detect.Detection`
     """
 
-    def __init__(self, class_id, bbox, difficult=False, occluded=False, truncated=False,
-                 iscrowd=False, area=None, confidence=None, image_id=None, task=None):
+    def __init__(self, class_id, bbox, include_margin=True, difficult=False, occluded=False,
+                 truncated=False, iscrowd=False, confidence=None, image_id=None, task=None):
         """Receives and stores low-level input detection metadata for later access."""
         self.class_id = class_id  # should be string or int to allow batching in data loaders
         # note: the input bbox is expected to be a 4 element array (xmin,ymin,xmax,ymax)
+        self.include_margin = include_margin
         self.bbox = bbox
         self.difficult = difficult
         self.occluded = occluded
         self.truncated = truncated
         self.iscrowd = iscrowd
-        self.area = area
         self.confidence = confidence
         self.image_id = image_id  # should be string that identifies the associated image (file path or uuid)
         self.task = task
@@ -80,7 +80,29 @@ class BoundingBox:
         assert not isinstance(value, (list, tuple)) or all([isinstance(v, (int, float)) for v in value]), \
             "input bbox values must be integer/float"
         assert value[0] <= value[2] and value[1] <= value[3], "invalid min/max values for bbox coordinates"
+        assert not self.include_margin or not any([isinstance(v, float) for v in value]), \
+            "it makes no sense to include xmax/ymax margin if using floating point coordinates"
         self._bbox = value
+
+    @property
+    def left(self):
+        """Returns the left bounding box edge origin offset value."""
+        return self._bbox[0]
+
+    @left.setter
+    def left(self, value):
+        """Sets the left bounding box edge origin offset value."""
+        self._bbox[0] = value
+
+    @property
+    def top(self):
+        """Returns the top bounding box edge origin offset value."""
+        return self._bbox[1]
+
+    @top.setter
+    def top(self, value):
+        """Sets the top bounding box edge origin offset value."""
+        self._bbox[1] = value
 
     @property
     def top_left(self):
@@ -92,6 +114,26 @@ class BoundingBox:
         """Sets the top left bounding box corner coordinates (x,y)."""
         assert isinstance(value, (list, tuple, np.ndarray, torch.Tensor)) and len(value) == 2, "invalid input type/len"
         self._bbox[0], self._bbox[1] = value[0], value[1]
+
+    @property
+    def right(self):
+        """Returns the right bounding box edge origin offset value."""
+        return self._bbox[2]
+
+    @right.setter
+    def right(self, value):
+        """Sets the right bounding box edge origin offset value."""
+        self._bbox[2] = value
+
+    @property
+    def bottom(self):
+        """Returns the bottom bounding box edge origin offset value."""
+        return self._bbox[3]
+
+    @bottom.setter
+    def bottom(self, value):
+        """Sets the bottom bounding box edge origin offset value."""
+        self._bbox[3] = value
 
     @property
     def bottom_right(self):
@@ -108,19 +150,35 @@ class BoundingBox:
     @property
     def width(self):
         """Returns the width of the bounding box."""
-        return self._bbox[2] - self._bbox[0]
+        return (self._bbox[2] - self._bbox[0]) + 1 if self.include_margin else 0
 
     @property
     def height(self):
         """Returns the height of the bounding box."""
-        return self._bbox[3] - self._bbox[1]
+        return (self._bbox[3] - self._bbox[1]) + 1 if self.include_margin else 0
 
     @property
     def centroid(self, floor=False):
         """Returns the bounding box centroid coordinates (x,y)."""
-        if floor:
-            return (self._bbox[0] + self._bbox[2]) // 2, (self._bbox[1] + self._bbox[3]) // 2
-        return (self._bbox[0] + self._bbox[2]) / 2, (self._bbox[1] + self._bbox[3]) / 2
+        if self.include_margin:
+            if floor:
+                return (self._bbox[0] + self._bbox[2] + 1) // 2, (self._bbox[1] + self._bbox[3] + 1) // 2
+            return (self._bbox[0] + self._bbox[2] + 1) / 2, (self._bbox[1] + self._bbox[3] + 1) / 2
+        else:
+            if floor:
+                return (self._bbox[0] + self._bbox[2]) // 2, (self._bbox[1] + self._bbox[3]) // 2
+            return (self._bbox[0] + self._bbox[2]) / 2, (self._bbox[1] + self._bbox[3]) / 2
+
+    @property
+    def include_margin(self):
+        """Returns whether xmax/ymax is included in the bounding box area or not"""
+        return self._include_margin
+
+    @include_margin.setter
+    def include_margin(self, value):
+        """Sets whether xmax/ymax is included in the bounding box area or not"""
+        assert isinstance(value, (int, bool)), "flag type must be integer or boolean"
+        self._include_margin = value
 
     @property
     def difficult(self):
@@ -169,16 +227,7 @@ class BoundingBox:
     @property
     def area(self):
         """Returns a scalar indicating the total surface of the annotation (may be None if unknown/unspecified)."""
-        return self._area
-
-    @area.setter
-    def area(self, value):
-        if value is None:
-            value = self.width * self.height
-        else:
-            # may be assigned if for example we are using relative coords but want to keep track of the original surface
-            assert (isinstance(value, (int, float)) and value > 0), "invalid annotation surface area value"
-        self._area = value
+        return self.width * self.height
 
     @property
     def confidence(self):
@@ -210,8 +259,9 @@ class BoundingBox:
     @task.setter
     def task(self, value):
         """Sets the reference to the task object that holds extra metadata regarding the content of the bbox."""
-        assert isinstance(value, Detection), "task should be detection-related"
-        assert self.class_id in value.class_indices.values(), f"cannot find class_id '{self.class_id}' in task indices"
+        if value is not None:
+            assert isinstance(value, Detection), "task should be detection-related"
+            assert self.class_id in value.class_indices.values(), f"cannot find class_id '{self.class_id}' in task indices"
         self._task = value
 
     def encode(self, format=None):
@@ -226,8 +276,8 @@ class BoundingBox:
                     self.difficult, self.occluded, self.truncated]
         else:
             assert format is None, "unrecognized/unknown encoding format"
-            vec = [*self.bbox, self.class_id, self.difficult, self.occluded,
-                   self.truncated, self.iscrowd, self.area, self.image_id]
+            vec = [*self.bbox, self.class_id, self.include_margin, self.difficult, self.occluded,
+                   self.truncated, self.iscrowd, self.image_id]
             if self.confidence is not None:
                 vec += [self.confidence] if isinstance(self.confidence, float) else [*self.confidence]
             return vec
@@ -244,19 +294,30 @@ class BoundingBox:
             return BoundingBox(class_id=vec[4], bbox=vec[0:4], difficult=vec[5], occluded=vec[6], truncated=vec[7])
         else:
             assert format is None, "unrecognized/unknown encoding format"
-            assert len(vec) >= 11, "unexpected vector length (should contain 9 values or more)"
-            return BoundingBox(class_id=vec[4], bbox=vec[0:4], difficult=vec[5], occluded=vec[6],
-                               truncated=vec[7], iscrowd=vec[8], area=vec[9],
+            assert len(vec) >= 12, "unexpected vector length (should contain 9 values or more)"
+            return BoundingBox(class_id=vec[4], bbox=vec[0:4], include_margin=vec[5], difficult=vec[6],
+                               occluded=vec[7], truncated=vec[8], iscrowd=vec[9],
                                confidence=(None if len(vec) == 11 else vec[11:]),
                                image_id=vec[10], task=None)
+
+    def intersects(self, geom):
+        """Returns whether the bounding box intersects a geometry (i.e. a 2D point or another bbox)."""
+        assert isinstance(geom, (tuple, list, np.ndarray, BoundingBox)), "unexpected input geometry type"
+        if isinstance(geom, (tuple, list, np.ndarray)):
+            # check intersection with point
+            assert len(geom) == 2, "point should be given as list of coordinates (x,y)"
+            return self._bbox[0] <= geom[0] <= self.bbox[2] and self._bbox[1] <= geom[1] <= self.bbox[3]
+        else:
+            return not (self._bbox[0] > geom._bbox[2] or geom._bbox[0] > self._bbox[2] or
+                        self._bbox[3] < geom._bbox[1] or geom._bbox[3] < self._bbox[1])
 
     def __repr__(self):
         """Creates a print-friendly representation of the object detection bbox instance."""
         # note: we do not export the task reference here (it might be too heavy for logs)
         return self.__class__.__module__ + "." + self.__class__.__qualname__ + \
-            f"(class_id={repr(self.class_id)}, bbox={repr(self.bbox)}, difficult={repr(self.difficult)}, " + \
-            f"occluded={repr(self.occluded)}, truncated={repr(self.truncated)}, iscrowd={repr(self.iscrowd)}, " + \
-            f"area={repr(self.area)}, confidence={repr(self.confidence)}, image_id={repr(self.image_id)})"
+            f"(class_id={repr(self.class_id)}, bbox={repr(self.bbox)}, include_margin={repr(self.include_margin)}, " + \
+            f"difficult={repr(self.difficult)}, occluded={repr(self.occluded)}, truncated={repr(self.truncated)}, " + \
+            f"iscrowd={repr(self.iscrowd)}, confidence={repr(self.confidence)}, image_id={repr(self.image_id)})"
 
 
 class Detection(Regression):
