524f04e6d3d9a7917df092d289720d07ab3dc9ab
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-10-28 20:45:58 +0000

    Cleanup coordconv implementation (add util func)

--- thelper/nn/coordconv.py
+++ thelper/nn/coordconv.py
@@ -2,56 +2,44 @@ import torch
 import torch.nn
 
 
+def get_coords_map(height, width, centered=True, normalized=True, noise=None, dtype=torch.float32):
+    """Returns a HxW intrinsic coordinates map (2xHxW)."""
+    x = torch.arange(width, dtype=dtype).unsqueeze(0)
+    y = torch.arange(height, dtype=dtype).unsqueeze(0)
+    if centered:
+        x -= (width - 1) // 2
+        y -= (height - 1) // 2
+    if normalized:
+        x /= width - 1
+        y /= height - 1
+    x = x.repeat(height, 1)
+    y = y.t().repeat(1, width)
+    if noise is not None:
+        assert isinstance(noise, float) and noise >= 0, "invalid noise stddev value"
+        x = torch.normal(mean=x, std=noise)
+        y = torch.normal(mean=y, std=noise)
+    return torch.stack([x, y])
+
+
 class AddCoords(torch.nn.Module):
-    def __init__(self, radius_channel=False):
+    def __init__(self, centered=True, normalized=True, noise=None, radius_channel=False):
         super().__init__()
+        self.centered = centered
+        self.normalized = normalized
+        self.noise = noise
         self.radius_channel = radius_channel
 
     def forward(self, in_tensor):
-        """
-        in_tensor: (batch_size, channels, x_dim, y_dim)
-        [0,0,0,0]   [0,1,2,3]
-        [1,1,1,1]   [0,1,2,3]    << (i,j)th coordinates of pixels added as separate channels
-        [2,2,2,2]   [0,1,2,3]
-        taken from mkocabas.
-        """
-        batch_size_tensor = in_tensor.shape[0]
-
-        xx_ones = torch.ones([1, in_tensor.shape[2]], dtype=torch.int32)
-        xx_ones = xx_ones.unsqueeze(-1)
-
-        xx_range = torch.arange(in_tensor.shape[2], dtype=torch.int32).unsqueeze(0)
-        xx_range = xx_range.unsqueeze(1)
-
-        xx_channel = torch.matmul(xx_ones, xx_range)
-        xx_channel = xx_channel.unsqueeze(-1)
-
-        yy_ones = torch.ones([1, in_tensor.shape[3]], dtype=torch.int32)
-        yy_ones = yy_ones.unsqueeze(1)
-
-        yy_range = torch.arange(in_tensor.shape[3], dtype=torch.int32).unsqueeze(0)
-        yy_range = yy_range.unsqueeze(-1)
-
-        yy_channel = torch.matmul(yy_range, yy_ones)
-        yy_channel = yy_channel.unsqueeze(-1)
-
-        xx_channel = xx_channel.permute(0, 3, 1, 2)
-        yy_channel = yy_channel.permute(0, 3, 1, 2)
-
-        xx_channel = xx_channel.float() / (in_tensor.shape[2] - 1)
-        yy_channel = yy_channel.float() / (in_tensor.shape[3] - 1)
-
-        xx_channel = xx_channel * 2 - 1
-        yy_channel = yy_channel * 2 - 1
-
-        xx_channel = xx_channel.repeat(batch_size_tensor, 1, 1, 1)
-        yy_channel = yy_channel.repeat(batch_size_tensor, 1, 1, 1)
-
-        dev = in_tensor.device
-        out = torch.cat([in_tensor, xx_channel.to(dev), yy_channel.to(dev)], dim=1)
+        batch_size, channels, height, width = in_tensor.shape
+        coords_map = get_coords_map(height, width, self.centered, self.normalized, self.noise)
         if self.radius_channel:
-            radius_calc = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
-            out = torch.cat([out, radius_calc.to(dev)], dim=1)
+            middle_slice = coords_map[:, (height - 1) // 2, (width - 1) // 2]
+            radius = torch.sqrt(torch.pow(coords_map[0, :, :] - middle_slice[0], 2) +
+                                torch.pow(coords_map[1, :, :] - middle_slice[1], 2))
+            coords_map = torch.cat([coords_map, radius.unsqueeze(0)], dim=0)
+        coords_map = coords_map.repeat(batch_size, 1, 1, 1)
+        dev = in_tensor.device
+        out = torch.cat([in_tensor, coords_map.to(dev)], dim=1)
         return out
 
 
