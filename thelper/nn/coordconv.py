7bfd08cf06b8ce5d6b5f6f3e6f8bae429f611526
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-10-29 22:24:25 +0000

    Add coordconv model layer swap util function

--- thelper/nn/coordconv.py
+++ thelper/nn/coordconv.py
@@ -1,3 +1,5 @@
+import collections
+
 import torch
 import torch.nn
 
@@ -23,16 +25,19 @@ def get_coords_map(height, width, centered=True, normalized=True, noise=None, dt
 
 class AddCoords(torch.nn.Module):
     """Creates a torch-compatible layer that adds intrinsic coordinate layers to input tensors."""
-    def __init__(self, centered=True, normalized=True, noise=None, radius_channel=False):
+    def __init__(self, centered=True, normalized=True, noise=None, radius_channel=False, scale=None):
         super().__init__()
         self.centered = centered
         self.normalized = normalized
         self.noise = noise
         self.radius_channel = radius_channel
+        self.scale = None
 
     def forward(self, in_tensor):
         batch_size, channels, height, width = in_tensor.shape
         coords_map = get_coords_map(height, width, self.centered, self.normalized, self.noise)
+        if self.scale is not None:
+            coords_map *= self.scale
         if self.radius_channel:
             middle_slice = coords_map[:, (height - 1) // 2, (width - 1) // 2]
             radius = torch.sqrt(torch.pow(coords_map[0, :, :] - middle_slice[0], 2) +
@@ -52,10 +57,10 @@ class CoordConv2d(torch.nn.Module):
     """
 
     def __init__(self, in_channels, *args, centered=True, normalized=True,
-                 noise=None, radius_channel=False, **kwargs):
+                 noise=None, radius_channel=False, scale=None, **kwargs):
         super().__init__()
-        self.addcoord = AddCoords(centered=centered, normalized=normalized,
-                                  noise=noise, radius_channel=radius_channel)
+        self.addcoord = AddCoords(centered=centered, normalized=normalized, noise=noise,
+                                  radius_channel=radius_channel, scale=scale)
         extra_ch = 3 if radius_channel else 2
         self.conv = torch.nn.Conv2d(in_channels + extra_ch, *args, **kwargs)
 
@@ -73,10 +78,10 @@ class CoordConvTranspose2d(torch.nn.Module):
     """
 
     def __init__(self, in_channels, *args, centered=True, normalized=True,
-                 noise=None, radius_channel=False, **kwargs):
+                 noise=None, radius_channel=False, scale=None, **kwargs):
         super().__init__()
-        self.addcoord = AddCoords(centered=centered, normalized=normalized,
-                                  noise=noise, radius_channel=radius_channel)
+        self.addcoord = AddCoords(centered=centered, normalized=normalized, noise=noise,
+                                  radius_channel=radius_channel, scale=scale)
         extra_ch = 3 if radius_channel else 2
         self.convT = torch.nn.ConvTranspose2d(in_channels + extra_ch, *args, **kwargs)
 
@@ -84,3 +89,36 @@ class CoordConvTranspose2d(torch.nn.Module):
         out = self.addcoord(in_tensor)
         out = self.convT(out)
         return out
+
+
+def swap_coordconv_layers(module, centered=True, normalized=True,
+                          noise=None, radius_channel=False, scale=None):
+    """Modifies the provided module by swapping Conv2d layers for CoordConv-equivalent layers."""
+    # note: this is a pretty 'dumb' way to add coord maps to a model, as it will add them everywhere, even
+    # in a potential output (1x1) conv layer; manually designing the network with these would be more adequate!
+    coordconv_params = {"centered": centered, "normalized": normalized, "noise": noise,
+                        "radius_channel": radius_channel, "scale": scale}
+    if isinstance(module, torch.nn.Conv2d):
+        return CoordConv2d(in_channels=module.in_channels, out_channels=module.out_channels,
+                           kernel_size=module.kernel_size, stride=module.stride, padding=module.padding,
+                           dilation=module.dilation, groups=module.groups, bias=module.bias is not None,
+                           padding_mode=module.padding_mode, **coordconv_params)
+    elif isinstance(module, torch.nn.ConvTranspose2d):
+        return CoordConvTranspose2d(in_channels=module.in_channels, out_channels=module.out_channels,
+                                    kernel_size=module.kernel_size, stride=module.stride,
+                                    padding=module.padding, output_padding=module.output_padding,
+                                    groups=module.groups, bias=module.bias is not None,
+                                    dilation=module.dilation, padding_mode=module.padding_mode,
+                                    **coordconv_params)
+    elif isinstance(module, torch.nn.Sequential):
+        return torch.nn.Sequential(*[swap_coordconv_layers(m, **coordconv_params) for m in module])
+    elif isinstance(module, collections.OrderedDict):
+        for mname, m in module.items():
+            module[mname] = swap_coordconv_layers(m, **coordconv_params)
+    elif isinstance(module, torch.nn.Module):
+        for attrib, m in module.__dict__.items():
+            if isinstance(m, (torch.nn.Conv2d, torch.nn.ConvTranspose2d)):
+                setattr(module, attrib, swap_coordconv_layers(m, **coordconv_params))
+            elif isinstance(m, (torch.nn.Module, collections.OrderedDict)):
+                setattr(module, attrib, swap_coordconv_layers(m, **coordconv_params))
+    return module
