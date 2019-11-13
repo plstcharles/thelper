28a227b137b1a09ed4be6a8f9fc7c4a017252d7e
Pierre-Luc St-Charles <stcharpl@crim.ca>
2019-11-13 23:30:03 +0000

    Add fixed weight sampler & clean asserts in others

--- thelper/data/samplers.py
+++ thelper/data/samplers.py
@@ -6,9 +6,11 @@ All samplers here should aim to be compatible with PyTorch's sampling interface
 (torch.utils.data.sampler.Sampler) so that they can be instantiated at runtime
 through a configuration file and used as the input of a data loader.
 """
+import collections
 import copy
 import logging
 
+import numpy as np
 import torch
 import torch.utils.data.sampler
 
@@ -113,19 +115,15 @@ class WeightedSubsetRandomSampler(torch.utils.data.sampler.Sampler):
             epoch: epoch number used to reinitialize the RNG to an epoch-specific state.
         """
         super().__init__(None)
-        if not isinstance(indices, list) or not isinstance(labels, list):
-            raise AssertionError("expected indices and labels to be provided as lists")
-        if len(indices) != len(labels):
-            raise AssertionError("mismatched indices/labels list sizes")
-        if not isinstance(scale, float) or scale < 0:
-            raise AssertionError("invalid scale parameter; should be greater than zero")
+        assert isinstance(indices, (list, np.ndarray)) and isinstance(labels, (list, np.ndarray)), \
+            "expected indices and labels to be provided as lists"
+        assert len(indices) == len(labels), "mismatched indices/labels list sizes"
+        assert isinstance(scale, float) and scale >= 0, "invalid scale parameter; should be greater than zero"
         self.seeds = {}
         if seeds is not None:
-            if not isinstance(seeds, dict):
-                raise AssertionError("unexpected seed pack type")
+            assert isinstance(seeds, dict), "unexpected seed pack type"
             self.seeds = seeds
-        if not isinstance(epoch, int) or epoch < 0:
-            raise AssertionError("invalid epoch value")
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
         self.epoch = epoch
         self.nb_samples = int(round(len(indices) * scale))
         if self.nb_samples > 0:
@@ -137,15 +135,15 @@ class WeightedSubsetRandomSampler(torch.utils.data.sampler.Sampler):
                     self.label_groups[label].append(indices[idx])
                 else:
                     self.label_groups[label] = [indices[idx]]
-            if not isinstance(stype, str) or (stype not in ["uniform", "random"] and "root" not in stype):
-                raise AssertionError("unexpected sampling type")
+            assert isinstance(stype, str) and (stype in ["uniform", "random"] or "root" in stype), \
+                "unexpected sampling type"
             if stype == "random":
                 self.sample_weights = [1.0 / len(self.label_groups[label]) for label in labels]
             else:
                 weights = thelper.data.utils.get_class_weights(self.label_groups, stype, invmax=False)
                 self.label_counts = {}
                 curr_nb_samples, max_sample_label = 0, None
-                for label_idx, (label, indices) in enumerate(self.label_groups.items()):
+                for label_idx, (label, _) in enumerate(self.label_groups.items()):
                     self.label_counts[label] = int(self.nb_samples * weights[label])
                     curr_nb_samples += self.label_counts[label]
                     if max_sample_label is None or len(self.label_groups[label]) > len(self.label_groups[max_sample_label]):
@@ -155,8 +153,7 @@ class WeightedSubsetRandomSampler(torch.utils.data.sampler.Sampler):
 
     def set_epoch(self, epoch=0):
         """Sets the current epoch number in order to offset the RNG state for sampling."""
-        if not isinstance(epoch, int) or epoch < 0:
-            raise AssertionError("invalid epoch value")
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
         self.epoch = epoch
 
     def __iter__(self):
@@ -183,14 +180,12 @@ class WeightedSubsetRandomSampler(torch.utils.data.sampler.Sampler):
         elif self.stype == "uniform" or "root" in self.stype:
             indices = []
             for label, count in self.label_counts.items():
-                max_samples = len(self.label_groups[label])
                 while count > 0:
-                    subidxs = torch.randperm(max_samples)
-                    for subidx in range(min(count, max_samples)):
+                    subidxs = torch.randperm(len(self.label_groups[label]))
+                    for subidx in range(min(count, len(subidxs))):
                         indices.append(self.label_groups[label][subidxs[subidx]])
-                    count -= max_samples
-            if len(indices) != self.nb_samples:
-                raise AssertionError("messed up something internally...")
+                    count -= len(subidxs)
+            assert len(indices) == self.nb_samples, "messed up something internally..."
             result = (indices[i] for i in torch.randperm(len(indices)))
         if rng_state is not None:
             torch.random.set_rng_state(rng_state)
@@ -222,21 +217,17 @@ class SubsetRandomSampler(torch.utils.data.sampler.Sampler):
         super().__init__(indices)
         self.seeds = {}
         if seeds is not None:
-            if not isinstance(seeds, dict):
-                raise AssertionError("unexpected seed pack type")
+            assert isinstance(seeds, dict), "unexpected seed pack type"
             self.seeds = seeds
-        if not isinstance(epoch, int) or epoch < 0:
-            raise AssertionError("invalid epoch value")
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
         self.epoch = epoch
         self.indices = indices
-        if not isinstance(scale, (float, int)) or scale < 0:
-            raise AssertionError("invalid scale parameter; should be greater than zero")
+        assert isinstance(scale, float) and scale >= 0, "invalid scale parameter; should be greater than zero"
         self.num_samples = int(round(len(self.indices) * scale))
 
     def set_epoch(self, epoch=0):
         """Sets the current epoch number in order to offset the RNG state for sampling."""
-        if not isinstance(epoch, int) or epoch < 0:
-            raise AssertionError("invalid epoch value")
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
         self.epoch = epoch
 
     def __iter__(self):
@@ -278,3 +269,130 @@ class SubsetSequentialSampler(torch.utils.data.sampler.Sampler):
 
     def __len__(self):
         return len(self.indices)
+
+
+class FixedWeightSubsetSampler(torch.utils.data.sampler.Sampler):
+    r"""Provides a rebalanced list of sample indices to use in a data loader.
+
+    Given a list of sample indices and the corresponding list of class labels, this sampler
+    will produce a new list of indices that rebalances the distribution of samples according
+    to a provided array of weights.
+
+    Example configuration file::
+
+        # ...
+        # the sampler is defined inside the 'loaders' field
+        "loaders": {
+            # ...
+            # this field is completely optional, and can be omitted entirely
+            "sampler": {
+                # the type of the sampler we want to instantiate
+                "type": "thelper.data.samplers.FixedWeightSubsetSampler",
+                # the parameters passed to the sampler's constructor
+                "params": {
+                    "weights": {
+                        # the weights must be provided using class name pairs
+                        "class_A": 0.1,
+                        "class_B": 5.0,
+                        "class_C": 1.0,
+                        # ...
+                    }
+                },
+                # specifies whether the sampler should receive class labels
+                "pass_labels": true
+            },
+            # ...
+        },
+        # ...
+
+    Attributes:
+        nb_samples: total number of samples to rebalance (i.e. scaled size of original dataset).
+        weights: weight map to use for sampling each class
+        indices: copy of the original list of sample indices provided in the constructor.
+        seeds: dictionary of seeds to use when initializing RNG state.
+        epoch: epoch number used to reinitialize the RNG to an epoch-specific state.
+
+    .. seealso::
+        | :func:`thelper.data.utils.create_loaders`
+        | :func:`thelper.data.utils.get_class_weights`
+    """
+
+    def __init__(self, indices, labels, weights, seeds=None, epoch=0):
+        """Receives sample indices, labels, rebalancing strategy, and dataset scaling factor.
+
+        This function will validate all input arguments, parse and categorize samples according to
+        labels, initialize rebalancing parameters, and determine sample counts for each valid class.
+        Note that an empty list of indices is an acceptable input; the resulting object will also
+        create and empty list of samples when ``__iter__`` is called.
+
+        Args:
+            indices: list of integers representing the indices of samples of interest in the dataset.
+            labels: list of labels tied to the list of indices (must be the same length).
+            weights: weight map to use for sampling each class.
+            seeds: dictionary of seeds to use when initializing RNG state.
+            epoch: epoch number used to reinitialize the RNG to an epoch-specific state.
+        """
+        super().__init__(None)
+        assert isinstance(indices, (list, np.ndarray)) and isinstance(labels, (list, np.ndarray)), \
+            "expected indices and labels to be provided as lists"
+        assert len(indices) == len(labels), "mismatched indices/labels list sizes"
+        self.seeds = {}
+        if seeds is not None:
+            assert isinstance(seeds, dict), "unexpected seed pack type"
+            self.seeds = seeds
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
+        self.epoch = epoch
+        assert isinstance(weights, (dict, collections.OrderedDict)), "invalid weights map type"
+        assert all([weight >= 0 for weight in weights.values()]), "weights must all be non-negative"
+        self.weights = weights
+        self.label_groups = {}
+        for idx, label in enumerate(labels):
+            if label in self.label_groups:
+                self.label_groups[label].append(indices[idx])
+            else:
+                self.label_groups[label] = [indices[idx]]
+        self.class_sample_counts = {label: int(round(self.weights[label] * len(self.label_groups[label])))
+                                    for label in self.label_groups}
+        self.nb_samples = sum(self.class_sample_counts.values())
+
+    def set_epoch(self, epoch=0):
+        """Sets the current epoch number in order to offset the RNG state for sampling."""
+        assert isinstance(epoch, int) and epoch >= 0, "invalid epoch index value"
+        self.epoch = epoch
+
+    def __iter__(self):
+        """Returns the list of rebalanced sample indices to load.
+
+        Note that the indices are repicked every time this function is called, meaning that samples
+        eliminated due to undersampling (or duplicated due to oversampling) might not receive the same
+        treatment twice.
+
+        This function will reseed the RNGs it uses every time it is called, and revert their state before
+        returning its output.
+        """
+        if self.nb_samples == 0:
+            self.epoch += 1
+            return iter([])
+        rng_state = None
+        if "torch" in self.seeds:
+            rng_state = torch.random.get_rng_state()
+            torch.random.manual_seed(self.seeds["torch"] + self.epoch)
+        result = []
+        for label, group in self.label_groups.items():
+            count_to_sample = self.class_sample_counts[label]
+            while count_to_sample > 0:
+                perm_size = min(count_to_sample, len(group))
+                result += [group[idx] for idx in torch.randperm(len(group))[0:perm_size].tolist()]
+                count_to_sample -= perm_size
+        assert len(result) == self.nb_samples
+        if rng_state is not None:
+            torch.random.set_rng_state(rng_state)
+        self.epoch += 1
+        return iter(result)
+
+    def __len__(self):
+        """Returns the number of sample indices that will be generated by this interface.
+
+        This number is the scaled size of the originally provided sample indices list.
+        """
+        return self.nb_samples
