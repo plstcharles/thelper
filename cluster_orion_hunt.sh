cfdace3d8eb6e996b5f875058151dbb73b6636d2
Pierre-Luc St-Charles <pierreluc.stcharles@mila.quebec>
2020-04-13 12:31:18 -0400

    Fixup orion python env lookup w/ conda

--- cluster_orion_hunt.sh
+++ cluster_orion_hunt.sh
@@ -27,5 +27,5 @@ conda activate thelper
 export ORION_DB_ADDRESS='sessions/orion/database.pkl'
 export ORION_DB_TYPE='pickleddb'
 
-"$CONDA_PREFIX/bin/orion" hunt --config configs/orion.yml --worker-trials 1 --working-dir 'sessions/orion' thelper/cli.py new --config 'configs/agrivis-ae.yml' --save-dir 'sessions/orion/trial_{trial.id}/'
+"$CONDA_PREFIX/bin/orion" hunt --config configs/orion.yml --worker-trials 1 --working-dir 'sessions/orion' "$CONDA_PREFIX/bin/python" thelper/cli.py new --config 'configs/agrivis-ae.yml' --save-dir 'sessions/orion/trial_{trial.id}/'
 
