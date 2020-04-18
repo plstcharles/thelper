f93eac05c5eb6400c697f6050f433ea74f0dd16d
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-18 14:08:42 -0400

    Cleanup import ordering in infer init

--- thelper/infer/__init__.py
+++ thelper/infer/__init__.py
@@ -6,7 +6,7 @@ This package contains classes specialized for inference of models on various tas
 import logging
 
 from thelper.infer.base import Tester  # noqa: F401
-from thelper.infer.impl import ImageClassifTester, ObjDetectTester, RegressionTester, ImageSegmTester  # noqa: F401
+from thelper.infer.impl import ImageClassifTester, ImageSegmTester, ObjDetectTester, RegressionTester  # noqa: F401
 from thelper.infer.utils import create_tester  # noqa: F401
 
 logger = logging.getLogger(__name__)
